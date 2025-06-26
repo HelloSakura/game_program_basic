2bbb748b95e9a00d8dafd4561f22e140dbef6393
duanhao303 <blight98@163.com>
2025-06-26 08:56:38 +0800

    add algorithm sort selection

---
+++ Algorithm/Sort/Insertion.cpp
@@ -0,0 +1,51 @@
+/*
+*@author lucida
+*description 插入排序
+*@date 2025-06-26
+*/
+#include<iostream>
+
+class Insertion{
+public:
+    Insertion(int* array, int size)
+    {
+        m_array = array;
+        m_size = size;
+    }
+
+    ~Insertion()
+    {
+        delete[] m_array;
+        m_array = nullptr;
+        m_size = 0;
+    }
+
+    /**
+     * @brief 循环遍历，将每一项插入到其它已经有序的牌中的适当位置
+     * 时间复杂度：O(N^2)
+     * 空间复杂度：O(1)
+     */
+    void sort()
+    {
+        for(int i = 1; i < m_size; i++){
+            int tmp = m_array[i];
+            int j = i - 1;
+            while(j >= 0 && m_array[j] > tmp){
+                m_array[j + 1] = m_array[j];
+                j--;
+            }
+            m_array[j + 1] = tmp;
+        }
+    }
+
+    void print()
+    {
+        for(int i = 0; i < m_size; i++){
+            std::cout << m_array[i] << " ";
+        }
+    }
+
+private:
+    int* m_array;
+    int m_size;
+};
---
+++ Algorithm/Sort/Selection.cpp
@@ -0,0 +1,52 @@
+/*
+*@author lucida
+*description 选择排序
+*@date 2025-06-26
+*/
+#include<iostream>
+
+class Selection{
+public:
+    Selection(int* array, int size)
+    {
+        m_array = array;
+        m_size = size;
+    }
+
+    ~Selection()
+    {
+        delete[] m_array;
+        m_array = nullptr;
+        m_size = 0;
+    }
+
+    /**
+     * @brief 循环遍历，每次从余下的数组中找到一个最小值，与排序好的部分末尾交换
+     * 时间复杂度：O(N^2)
+     * 空间复杂度：O(1)
+     */
+    void sort()
+    {
+        for(int i = 0; i < m_size; i++){
+            int min = m_array[i];
+            for(int j = i + 1; j < m_size; j++){
+                if(m_array[j] < min){
+                    min = m_array[j];
+                    m_array[j] = m_array[i];
+                    m_array[i] = min;
+                }
+            }
+        }
+    }
+
+    void print()
+    {
+        for(int i = 0; i < m_size; i++){
+            std::cout << m_array[i] << " ";
+        }
+    }
+
+private:
+    int* m_array;
+    int m_size;
+};
